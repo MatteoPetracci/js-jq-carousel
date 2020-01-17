c808b70785604a0c21df77b6891fded1029f9051
Matteo <matteop18@gmail.com>
2020-01-17 15:26:33 +0100

    inizio seconda funzione prev()

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Selezionare le classi next e prev che contengono le icone delle freccie e assegnargli una funziona che partirà al click dell'utente
+
 $(document).ready(
   function () {
     $('.next').click(
@@ -17,7 +18,7 @@ $(document).ready(
 
 // Assegnare a delle variabili il contenuto che hanno le img con la classe active e eseguire la stessa cosa per le icone con classe active.
 
-// con il metodo next  prendiamo  il fratello successivo dell'elemento
+// con il metodo next()  prendiamo  il fratello successivo dell'elemento
 
 function changeNext() {
   var imgActive = $('img.active');
@@ -26,9 +27,11 @@ function changeNext() {
   var nextIcon = iconActive.next();
   var firstImg = $('img.first');
   var firstIcon = $('i.first');
+  console.log(iconActive);
+  console.log(nextIcon);
 
   // Dettiamo la condizione che se in imgActive è contenuta la classe last rimuoviamo e aggiungiamo la classe active alle varie immagine in successione
-  
+
   if (imgActive.hasClass('last')) {
     imgActive.removeClass('active');
     firstImg.addClass('active');
@@ -40,4 +43,13 @@ function changeNext() {
     iconActive.removeClass('active');
     nextIcon.addClass('active');
   }
+}
+
+
+// grazie al metodo prev() ottieni il fratello immediatamente precedente di ciascun elemento al contrario di next()
+
+// grazie a questo metodo creare una funzione che permetta di scorrere all'indietro le immagini
+
+function changePrev() {
+  
 }
